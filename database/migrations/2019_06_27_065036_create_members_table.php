0a2bdf996c8fe72f87df761e708b52bdfb357f9f
Eric <eric@Ericde-iMac.local>
2019-06-27 14:54:24 +0800

    create member table

---
+++ database/migrations/2019_06_27_065036_create_members_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 50);
+            $table->string('address', 300);
+            $table->string('phone', 10);
+            $table->string('account', 50);
+            $table->string('password', 50);
+            $table->string('mail', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
